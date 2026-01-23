7f22fe70b44b1d32bc3b2effc320a0e22551781f
Luca Rainieri <l.rainieri@campus.unimib.it>
2026-01-23 15:12:49 +0100

    Added UserController.java

---
+++ backend/src/main/java/it/bicocca/eduquest/controller/UserController.java
@@ -0,0 +1,21 @@
+package it.bicocca.eduquest.controller;
+
+import it.bicocca.eduquest.dto.UserDTO;
+import it.bicocca.eduquest.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserServices userServices;
+
+    // POST: Crea un utente vero salvandolo nel DB
+    @PostMapping
+    public UserDTO createUser(@RequestBody UserDTO inputPayload) {
+        // Chiamiamo il servizio vero che hai appena scritto!
+        return userServices.createUser(inputPayload.getName(), inputPayload.getEmail());
+    }
+}
--- backend/src/main/java/it/bicocca/eduquest/dto/UserDTO.java
+++ backend/src/main/java/it/bicocca/eduquest/dto/UserDTO.java
@@ -8,7 +8,7 @@ public class UserDTO {
         this.name = name;
         this.email = email;
     }
-
+    
     // Getter e Setter
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
