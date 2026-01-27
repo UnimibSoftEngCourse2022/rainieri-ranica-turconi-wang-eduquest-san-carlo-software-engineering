f6dac8193c6ded224d4b8a4f06570dd8aa406931
Andrea <a.ranica@campus.unimib.it>
2026-01-27 12:02:13 +0100

    Added quiz creation and visualization from teacher dashboard

--- frontend/teacher-dashboard/script.js
+++ frontend/teacher-dashboard/script.js
@@ -5,9 +5,13 @@ const LOGIN_PAGE = "/login/";
 const TEACHER_ROLE = "TEACHER";
 
 const ALL_QUIZZES_ENDPOINT = "http://localhost:8080/api/quiz";
+const ADD_QUIZ_ENDPOINT = "http://localhost:8080/api/quiz";
 
 const errorDiv = "<div class='alert alert-danger' role='alert'>An error occoured during the loading of your quizzes, please try again layer</div>";
 const emptyQuizListDiv = "<div class='alert alert-warning' role='alert'>You don't have any quiz... create one now!</div>";
+
+const addQuizSuccessDiv = "<div class='alert alert-success' role='alert'>Quiz created succesfully!</div>";
+const addQuizErrorDiv = "<div class='alert alert-danger' role='alert'>An error occoured during the creation of the quiz</div>";
 
 let userData = null;
 
@@ -43,9 +47,10 @@ const getTeacherQuizzes = async () => {
 
     if (response.ok) {
       const quizzes = await response.json();
-      console.log(quizzes)
       if (quizzes.length == 0) {
         quizzesElement.innerHTML = emptyQuizListDiv;
+      } else {
+        quizzesElement.innerHTML = getQuizzesElement(quizzes);
       }
     } else {
       quizzesElement.innerHTML = errorDiv;
@@ -55,3 +60,41 @@ const getTeacherQuizzes = async () => {
     quizzesElement.innerHTML = errorDiv;
   }
 }
+
+const getQuizzesElement = (quizzes) => {
+  let element = "<h1>Your quizzes</h1>"
+  quizzes.forEach(quiz => {
+    console.log(quiz);
+    element += `<p>${quiz.title}</h1><button class="btn" onclick="">✏️</button>`
+  });
+  return element
+}
+
+document.getElementById("add-quiz-button").addEventListener('click', async () => {
+  const jwt = window.localStorage.getItem("token");
+  const title = document.getElementById("quiz-title-input").value;
+  const description = document.getElementById("quiz-description-input").value;
+  
+  const requestBody = {
+    title, description
+  };
+  console.log(requestBody);
+  const response = await fetch(ADD_QUIZ_ENDPOINT, {
+    method: "POST",
+    headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + jwt
+    },
+    body: JSON.stringify(requestBody)
+  });
+
+  const addQuizResultElement = document.getElementById("add-quiz-result");
+  if (response.ok) {
+    const r = await response.json();
+    console.log(r);
+    addQuizResultElement.innerHTML = addQuizSuccessDiv;
+  } else {
+    addQuizResultElement.innerHTML = addQuizErrorDiv;
+  }
+})
