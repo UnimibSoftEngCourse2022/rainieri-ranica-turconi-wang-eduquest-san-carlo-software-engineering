b59a1b895f1f1eb2518a55e02159f07e7f0eeae2
Luca Rainieri <l.rainieri@campus.unimib.it>
2026-01-31 16:35:46 +0100

    Implemented StudentStatsListener (changed name to UserStatsListener) and StudentStats

--- backend/src/main/java/it/bicocca/eduquest/domain/users/StudentStats.java
+++ backend/src/main/java/it/bicocca/eduquest/domain/users/StudentStats.java
@@ -2,25 +2,54 @@ package it.bicocca.eduquest.domain.users;
 
 import jakarta.persistence.Embeddable;
 
-@Embeddable // Link to Student
+@Embeddable
 public class StudentStats {
-	private int numberOfCompletedQuizzes = 0;
-	private int numberOfCompletedMissions = 0;
+	private int quizzesCompleted = 0;
+	private int totalAnswerGiven = 0;
+	private int totalCorrectAnswers = 0;
+	private double averageQuizzesScore = 0.0;
 	
 	public StudentStats() {
-		// Default constructor
+
 	}
 	
-	public int getNumberOfCompletedQuizzes() {
-		return numberOfCompletedQuizzes;
+	public void updateStats(double newQuizScore, int answersGiven, int correctAnswers) {
+		this.quizzesCompleted++;
+		this.totalAnswerGiven += answersGiven;
+		this.totalCorrectAnswers += correctAnswers;
+		this.averageQuizzesScore = ((this.averageQuizzesScore * this.quizzesCompleted) + newQuizScore) / this.quizzesCompleted;
 	}
-	public void setNumberOfCompletedQuizzes(int numberOfCompletedQuizzes) {
-		this.numberOfCompletedQuizzes = numberOfCompletedQuizzes;
+
+	public int getQuizzesCompleted() {
+		return quizzesCompleted;
 	}
-	public int getNumberOfCompletedMissions() {
-		return numberOfCompletedMissions;
+
+	public int getTotalAnswerGiven() {
+		return totalAnswerGiven;
 	}
-	public void setNumberOfCompletedMissions(int numberOfCompletedMissions) {
-		this.numberOfCompletedMissions = numberOfCompletedMissions;
+
+	public int getTotalCorrectAnswers() {
+		return totalCorrectAnswers;
 	}
+
+	public double getAverageQuizzesScore() {
+		return averageQuizzesScore;
+	}
+
+	public void setQuizzesCompleted(int quizzesCompleted) {
+		this.quizzesCompleted = quizzesCompleted;
+	}
+
+	public void setTotalAnswerGiven(int totalAnswerGiven) {
+		this.totalAnswerGiven = totalAnswerGiven;
+	}
+
+	public void setTotalCorrectAnswers(int totalCorrectAnswers) {
+		this.totalCorrectAnswers = totalCorrectAnswers;
+	}
+
+	public void setAverageQuizzesScore(double averageQuizzesScore) {
+		this.averageQuizzesScore = averageQuizzesScore;
+	}
+
 }
--- backend/src/main/java/it/bicocca/eduquest/services/listeners/UserStatsListener.java
+++
@@ -1,5 +0,0 @@
-package it.bicocca.eduquest.services.listeners;
-
-public class UserStatsListener {
-
-}
