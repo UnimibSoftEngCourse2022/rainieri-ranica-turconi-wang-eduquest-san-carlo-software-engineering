216fd2a3e6c369dd28410d5b372ea12f41eae8c4
tiaturconi <m.turconi8@campus.unimib.it>
2026-02-03 16:22:51 +0100

    Created TestRepository, TestAddDTO, TestDTO

---
+++ backend/src/main/java/it/bicocca/eduquest/dto/quiz/TestDTO.java
@@ -0,0 +1,51 @@
+package it.bicocca.eduquest.dto.quiz;
+
+import java.time.Duration;
+
+public class TestDTO {
+
+	private long id;        
+    private Duration maxDuration;
+    private int maxTries;
+    private QuizDTO quiz;
+    
+    public TestDTO(long id, Duration maxDuration, int maxTries, QuizDTO quiz) {
+    	this.id = id;
+        this.maxDuration = maxDuration;
+        this.maxTries = maxTries;
+        this.quiz = quiz;
+    }
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Duration getMaxDuration() {
+		return maxDuration;
+	}
+
+	public void setMaxDuration(Duration maxDuration) {
+		this.maxDuration = maxDuration;
+	}
+
+	public int getMaxTries() {
+		return maxTries;
+	}
+
+	public void setMaxTries(int maxTries) {
+		this.maxTries = maxTries;
+	}
+
+	public QuizDTO getQuiz() {
+		return quiz;
+	}
+
+	public void setQuiz(QuizDTO quiz) {
+		this.quiz = quiz;
+	}
+    
+}
