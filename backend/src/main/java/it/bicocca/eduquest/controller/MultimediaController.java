058aaab5abf6e23416dbebde08d39d43fa5aef6d
tiaturconi <m.turconi8@campus.unimib.it>
2026-02-06 00:03:00 +0100

    Added MultimediaController

---
+++ backend/src/main/java/it/bicocca/eduquest/controller/MultimediaController.java
@@ -0,0 +1,28 @@
+package it.bicocca.eduquest.controller;
+
+import it.bicocca.eduquest.services.MultimediaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.Map;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/api/media")
+@CrossOrigin(origins = "*")
+public class MultimediaController {
+
+    private final MultimediaService multimediaService;
+
+    public MultimediaController(MultimediaService multimediaService) {
+        this.multimediaService = multimediaService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<Map<String, String>> uploadFile(@RequestParam("file") MultipartFile file) {
+        String url = multimediaService.uploadMedia(file, "eduquest_uploads");
+        Map<String, String> response = new HashMap<>();
+        response.put("url", url);
+        return ResponseEntity.ok(response);
+    }
+}
