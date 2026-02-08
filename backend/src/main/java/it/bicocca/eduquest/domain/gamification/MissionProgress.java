a0b611f5d701e57203a5be3e626b16bd61cb91a3
Luca Rainieri <l.rainieri@campus.unimib.it>
2026-02-08 18:20:53 +0100

    Commit to pull

--- backend/src/main/java/it/bicocca/eduquest/domain/gamification/MissionProgress.java
+++ backend/src/main/java/it/bicocca/eduquest/domain/gamification/MissionProgress.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import java.time.*;
 
 @Entity
 public class MissionProgress {
